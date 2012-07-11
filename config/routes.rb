82617f2ced372a829e536c80b4d47cee83e99ab4
Filip Tepper <filip@tepper.pl>
2012-07-11 15:11:00 +0200

    full redmine 2.0 compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 match "timesheet/index", :to => "timesheet#index"
 match "timesheet/context_menu", :to => "timesheet#context_menu"
-match "timesheet/report.:format", :to => "timesheet#report"
+match "timesheet/report", :to => "timesheet#report", :as => "report_timesheet"
 match "timesheet/reset", :to => "timesheet#reset", :via => :delete
